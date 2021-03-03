a83b3e58f34cc533eee9f61ed9e948a5c557e1c6
liphone <974122$07@qq.com>
2021-03-03 13:08:04 +0800

    add x2coco.py commit in 2021-03-03 13:08:04 by 97412

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#pip freeze > requirements.txt
+git add .
+time_str=$(date "+%Y-%m-%d %H:%M:%S")
+git commit -m "${1} commit in ${time_str} by `whoami`"
+git push origin master
+exit
